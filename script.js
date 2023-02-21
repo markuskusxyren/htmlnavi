ddb06979a11267c5f3bbd3ade35726834d0f20fd
markuskusxyren <mxlfranco2020@plm.edu.ph>
2023-02-21 15:20:49 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+mapboxgl.accessToken =
+  'pk.eyJ1Ijoia3Vza3VzeHlyZW5uIiwiYSI6ImNsZWN4ampubzAxaDczcG16MXcwcWhhcDEifQ.9K3JBDAzq3Ru8riWg49zgw';
+
+navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
+  enableHighAccuracy: true,
+});
+
+function successLocation(position) {
+  setupMap([position.coords.longitude, position.coords.latitude]);
+}
+
+function errorLocation() {
+  setupMap([-2.24, 53.48]);
+}
+
+function setupMap(center) {
+  const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: center,
+    zoom: 15,
+  });
+
+  const nav = new mapboxgl.NavigationControl();
+  map.addControl(nav);
+
+  var directions = new MapboxDirections({
+    accessToken: mapboxgl.accessToken,
+  });
+
+  map.addControl(directions, 'top-left');
+
+  map.addControl(
+    new mapboxgl.GeolocateControl({
+      positionOptions: {
+        enableHighAccuracy: true,
+      },
+      // When active the map will receive updates to the device's location as it changes.
+      trackUserLocation: true,
+      // Draw an arrow next to the location dot to indicate which direction the device is heading.
+      showUserHeading: true,
+    })
+  );
+}
